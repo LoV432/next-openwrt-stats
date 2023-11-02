3e3d93cb508e5b4260be3c05b8f98f2e7f598f8c
LoV432 <monib619@gmail.com>
2023-11-02 15:05:21 +0500

    add dropdown and change name modal

--- app/components/user-cards/UserCard.client.tsx
+++ app/components/user-cards/UserCard.client.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { allSpeedStates } from '../boundaries/SpeedBoundarie.client';
+import { useRouter } from 'next/navigation';
 
 export default function UserCard({
 	name,
@@ -35,8 +36,8 @@ export default function UserCard({
 		<>
 			<div className="card card-side m-5 h-fit w-full bg-base-100 p-2 shadow-xl sm:w-[300px]">
 				<UserSpeed ip={ip} />
-				<figure className="w-1/4 px-2 py-4">
-					<img className="" src={`/${devicetype}.svg`} alt={devicetype} />
+				<figure className="relative flex w-1/4 items-center justify-center overflow-visible px-1 py-4">
+					<DropDown macaddress={macaddress} />
 				</figure>
 				<div className="card-body w-2/3 p-4">
 					<h2 className="card-title border-b border-white border-opacity-30 pb-2">
@@ -89,5 +90,110 @@ function UserSpeed({ ip }: { ip: string }) {
 		</p>
 	) : (
 		<></>
+	);
+}
+
+function DropDown({ macaddress }: { macaddress: string }) {
+	const [dropDownIsOpen, SetDropDownIsOpen] = useState(false);
+	const [macAddress] = useState(macaddress);
+	const [changeNameModalIsOpen, setChangeNamemodalIsOpen] = useState(false);
+
+	function toggleNameModal() {
+		setChangeNamemodalIsOpen(!changeNameModalIsOpen);
+	}
+
+	return (
+		<>
+			<button
+				onClick={() => SetDropDownIsOpen(true)}
+				className="btn m-0 h-fit w-full border-none bg-transparent p-0 hover:bg-transparent active:bg-transparent"
+			>
+				<img src="/android.svg" />
+			</button>
+			{dropDownIsOpen && (
+				<>
+					<div className="absolute left-0 top-0 z-30 h-20">
+						<ul className="menu rounded-box w-36 bg-base-200">
+							<li>
+								<p
+									onClick={() => {
+										toggleNameModal();
+										SetDropDownIsOpen(false);
+									}}
+								>
+									Change Name
+								</p>
+							</li>
+							<li>
+								<p>Change Icon</p>
+							</li>
+							<li>
+								<a>Delete</a>
+							</li>
+						</ul>
+					</div>
+					<div
+						onClick={() => {
+							SetDropDownIsOpen(false);
+						}}
+						className="fixed left-0 top-0 z-20"
+					></div>
+				</>
+			)}
+			{changeNameModalIsOpen && (
+				<NameChangePopUp
+					macAddress={macAddress}
+					toggleNameModal={toggleNameModal}
+				/>
+			)}
+		</>
+	);
+}
+
+function NameChangePopUp({
+	macAddress,
+	toggleNameModal
+}: {
+	macAddress: string;
+	toggleNameModal: () => void;
+}) {
+	let router = useRouter();
+	const changeNameValue = useRef<HTMLInputElement>(null);
+	function changeName() {
+		let newName = changeNameValue.current?.value;
+		fetch(`/api/edit/display-name`, {
+			body: JSON.stringify({
+				macAddress: macAddress,
+				displayName: newName
+			}),
+			method: 'POST'
+		});
+		toggleNameModal();
+		router.refresh();
+	}
+	return (
+		<dialog id="changeNameModal" className="modal" open>
+			<div className="modal-box">
+				<h3 className="pb-5 text-lg font-bold">Enter New Name</h3>
+				<input
+					ref={changeNameValue}
+					type="text"
+					placeholder="Name"
+					className="input input-bordered w-full"
+				/>
+				<button onClick={changeName} className="btn btn-primary mt-5 w-full">
+					Apply
+				</button>
+				<button
+					onClick={toggleNameModal}
+					className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+				>
+					✕
+				</button>
+			</div>
+			<div className="modal-backdrop bg-zinc-700 opacity-30">
+				<button onClick={toggleNameModal}>close</button>
+			</div>
+		</dialog>
 	);
 }
