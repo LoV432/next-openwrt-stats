af6dcf734ba2d677d4be5d6549a53ff276e3fb0c
LoV432 <monib619@gmail.com>
2023-11-02 18:20:47 +0500

    dont prerender during build

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Dashboard from './components/dashboard/Dashboard.server';
 import UserCards from './components/user-cards/UserCards.server';
 import RecoilBoundarie from './components/boundaries/RecoilBoundarie.client';
 import SpeedBoundarie from './components/boundaries/SpeedBoundarie.client';
+export const dynamic = 'force-dynamic';
 export default function Home() {
 	return (
 		<div className="main-container min-h-screen w-full bg-black text-white">
