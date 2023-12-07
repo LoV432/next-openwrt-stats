0537c3b30146342ef39d4e43d1a0109d51fab909
LoV432 <monib619@gmail.com>
2023-12-07 18:04:55 +0500

    add NOT NULL UNIQUE to mac_address

--- create-db.js
+++ create-db.js
@@ -15,12 +15,35 @@ db.transaction(() => {
 	name TEXT,
 	display_name TEXT,
 	ip TEXT,
-	mac_address TEXT,
+	mac_address TEXT NOT NULL UNIQUE,
 	last_updated INTEGER,
 	device_type TEXT,
 	last_event_type TEXT
 )`);
     createUserTable.run();
+
+    // DB migrations to "mac_address TEXT NOT NULL UNIQUE"
+    const checkTableInfo = db.pragma(`index_info(sqlite_autoindex_users_1)`);
+    if (checkTableInfo.length === 0) {
+        console.log('Migrating DB')
+        db.prepare(`CREATE TABLE IF NOT EXISTS users_duplicate (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            index_number INTEGER,
+            name TEXT,
+            display_name TEXT,
+            ip TEXT,
+            mac_address TEXT NOT NULL UNIQUE,
+            last_updated INTEGER,
+            device_type TEXT,
+            last_event_type TEXT
+        )`).run();
+        // Copy all data from users to users-duplicate
+        db.prepare('INSERT INTO users_duplicate SELECT * FROM users').run();
+        // Drop the original table
+        db.prepare('DROP TABLE users').run();
+        // Rename the duplicate table to the original name
+        db.prepare('ALTER TABLE users_duplicate RENAME TO users').run();
+    }
 
     const createConnectionLogsTable =
         db.prepare(`CREATE TABLE IF NOT EXISTS connectionlogs (
