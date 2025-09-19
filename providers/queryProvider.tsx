a775f2101d8e302680cfa79693eb9a17a76905f3
LoV432 <monib619@gmail.com>
2025-09-19 09:10:13 +0000

    add tanstack query devtool

--- providers/queryProvider.tsx
+++ providers/queryProvider.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
+	const queryClient = new QueryClient();
+
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+			{children}
+		</QueryClientProvider>
 	);
 }
