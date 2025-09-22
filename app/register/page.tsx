8e274c0ecea4a6d9a0a1f084bdbc5256b353cc4d
LoV432 <monib619@gmail.com>
2025-09-22 08:01:00 +0000

    update register page styles

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -13,8 +13,10 @@ import {
 	SelectTrigger,
 	SelectValue
 } from '@/components/ui/select';
+import Image from 'next/image';
 
 export default function Register() {
+	const [warningAccepted, setWarningAccepted] = useState('false');
 	const [displayName, setDisplayName] = useState('');
 	const [protocol, setProtocol] = useState('http://');
 	const [routerIP, setRouterIP] = useState('');
@@ -73,104 +75,135 @@ export default function Register() {
 					</p>
 				</div>
 
-				<div className="mb-8 rounded-lg border border-red-800 bg-red-950 p-4">
-					<div className="text-center">
-						<h2 className="mb-2 text-lg font-bold text-red-400">
-							⚠️ Security Warning
-						</h2>
-						<p className="text-sm leading-relaxed text-red-300">
-							This panel can make destructive changes to your router and comes
-							with no built-in authentication. You must put this panel behind
-							proper authentication before use.
-						</p>
-					</div>
-				</div>
-
-				<div className="rounded-lg border border-neutral-800 bg-neutral-900 p-6">
-					<form
-						className="space-y-4"
-						onSubmit={(e) => {
-							e.preventDefault();
-							register();
-						}}
-					>
-						<div>
-							<label className="mb-2 block text-sm font-medium text-neutral-300">
-								Display Name
-							</label>
-							<Input
-								className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
-								placeholder="e.g. Router 1, Main Router, Upstairs AP"
-								required
-								value={displayName}
-								onChange={(e) => setDisplayName(e.target.value)}
-							/>
-						</div>
-						<div>
-							<label className="mb-2 block text-sm font-medium text-neutral-300">
-								Router IP Address / Domain
-							</label>
-							<div className="flex items-center justify-center gap-2">
-								<Select
-									value={protocol}
-									defaultValue="http://"
-									onValueChange={(value) => setProtocol(value)}
-								>
-									<SelectTrigger className="min-h-11 min-w-[95px]">
-										<SelectValue placeholder="http://" />
-									</SelectTrigger>
-									<SelectContent>
-										<SelectItem value="http://">HTTP</SelectItem>
-										<SelectItem value="https://">HTTPS</SelectItem>
-									</SelectContent>
-								</Select>
-								<p className="text-neutral-300">://</p>
+				{warningAccepted === 'true' ? (
+					<div className="rounded-lg border border-neutral-800 bg-neutral-900 p-6">
+						<form
+							className="space-y-4"
+							onSubmit={(e) => {
+								e.preventDefault();
+								register();
+							}}
+						>
+							<div>
+								<label className="mb-2 block text-sm font-medium text-neutral-300">
+									Display Name
+								</label>
 								<Input
 									className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
-									placeholder="192.168.1.1"
+									placeholder="e.g. Router 1, Main Router, Upstairs AP"
 									required
-									value={routerIP}
-									onChange={(e) => setRouterIP(e.target.value)}
+									value={displayName}
+									onChange={(e) => setDisplayName(e.target.value)}
 								/>
+							</div>
+							<div>
+								<label className="mb-2 block text-sm font-medium text-neutral-300">
+									Router IP Address / Domain
+								</label>
+								<div className="flex items-center justify-center gap-2">
+									<Select
+										value={protocol}
+										defaultValue="http://"
+										onValueChange={(value) => setProtocol(value)}
+									>
+										<SelectTrigger className="min-h-11 min-w-[95px]">
+											<SelectValue placeholder="http://" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectItem value="http://">HTTP</SelectItem>
+											<SelectItem value="https://">HTTPS</SelectItem>
+										</SelectContent>
+									</Select>
+									<p className="text-neutral-300">://</p>
+									<Input
+										className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
+										placeholder="192.168.1.1"
+										required
+										value={routerIP}
+										onChange={(e) => setRouterIP(e.target.value)}
+									/>
+								</div>
+							</div>
+
+							<div>
+								<label className="mb-2 block text-sm font-medium text-neutral-300">
+									Username
+								</label>
+								<Input
+									className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
+									placeholder="root"
+									required
+									value={username}
+									onChange={(e) => setUsername(e.target.value)}
+								/>
+							</div>
+
+							<div>
+								<label className="mb-2 block text-sm font-medium text-neutral-300">
+									Password
+								</label>
+								<Input
+									className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
+									placeholder="Enter your router password"
+									required
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									type="password"
+								/>
+							</div>
+
+							<Button
+								className="mt-6 h-11 w-full font-medium"
+								variant={'outline'}
+								disabled={isLoading}
+							>
+								{isLoading ? 'Registering...' : 'Register Router'}
+							</Button>
+						</form>
+					</div>
+				) : warningAccepted === 'false' ? (
+					<>
+						<div className="rounded-lg border border-red-800 bg-red-950 p-4">
+							<div className="text-center">
+								<h2 className="mb-2 text-lg font-bold text-red-400">
+									⚠️ Security Warning
+								</h2>
+								<p className="text-sm leading-relaxed text-red-300">
+									This panel can make destructive changes to your router and
+									comes with no built-in authentication. You must put this panel
+									behind proper authentication and take backups of your router
+									before use.
+								</p>
 							</div>
 						</div>
 
-						<div>
-							<label className="mb-2 block text-sm font-medium text-neutral-300">
-								Username
-							</label>
-							<Input
-								className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
-								placeholder="root"
-								required
-								value={username}
-								onChange={(e) => setUsername(e.target.value)}
-							/>
+						<div className="flex justify-center gap-4">
+							<Button
+								className="mt-6 h-11 w-1/3 font-medium"
+								onClick={() => setWarningAccepted('cancel')}
+								variant={'outline'}
+							>
+								Nah, I'm out
+							</Button>
+							<Button
+								className="bg-red-800! mt-6 h-11 w-1/3 font-medium"
+								variant={'destructive'}
+								onClick={() => setWarningAccepted('true')}
+							>
+								I understand
+							</Button>
 						</div>
-
-						<div>
-							<label className="mb-2 block text-sm font-medium text-neutral-300">
-								Password
-							</label>
-							<Input
-								className="h-11 border-neutral-700 bg-neutral-800 text-white placeholder:text-neutral-500 focus:border-slate-600 focus:ring-slate-600"
-								placeholder="Enter your router password"
-								required
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								type="password"
-							/>
-						</div>
-
-						<Button
-							className="mt-6 h-11 w-full font-medium"
-							variant={'outline'}
-							disabled={isLoading}
-						>
-							{isLoading ? 'Registering...' : 'Register Router'}
-						</Button>
-					</form>
-				</div>
+					</>
+				) : (
+					<Image
+						src="/cancel.jpg"
+						alt="cancel"
+						width={400}
+						height={200}
+						loading="eager"
+						className="w-full"
+					/>
+				)}
 			</div>
 		</div>
 	);
