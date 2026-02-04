64df6c1216183cc7ed654c4354ac8719f7e31708
LoV432 <monib619@gmail.com>
2026-02-04 13:39:04 +0000

    update to next 16

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
 	if (process.env.LEGACY_DATABASE_DETECTED === 'true') {
 		return NextResponse.redirect(new URL('/legacy-migration', request.url));
 	}
